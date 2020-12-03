01e052bebf50d354bf58c4efc463026da6eb9fd3
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-03 06:10:47 -0500

    Uncommented solution for part 1.

--- 2020/Day 1/Advent1.py
+++ 2020/Day 1/Advent1.py
@@ -4,10 +4,10 @@ with open("expenses.txt") as expenses:
     for expense in expenses:
         expense_list.append(int(expense.rstrip("\n")))
 
-##for expense in expense_list:
-##    for expense1 in expense_list:
-##        if expense1 + expense == 2020:
-##            print(expense1 * expense)
+for expense in expense_list:
+    for expense1 in expense_list:
+        if expense1 + expense == 2020:
+            print(expense1 * expense)
 
 for expense in expense_list:
     for expense1 in expense_list:
