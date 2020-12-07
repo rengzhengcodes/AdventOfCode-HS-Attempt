85f55a147948142cf003b87ccaaa6a0e3031c4aa
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-07 03:06:56 -0500

    Solutions up to day 7.

--- 2020/Day 6/Advent6.py
+++ 2020/Day 6/Advent6.py
@@ -1,1 +1,48 @@
+import re
+
+groups = [];
+
 with open("input.txt") as f:
+    groups = list(f.read().split("\n\n"))
+
+for i in range(len(groups)):
+    groups[i] = list(groups[i].split("\n"))
+
+group_count = []
+
+for group in groups:
+    answers = ""
+    group_sum = 0;
+    for person in group:
+        person_sum = 0;
+        for answer in person:
+            if answer not in answers:
+                person_sum += 1
+                answers += answer
+        group_sum += person_sum
+    group_count.append(group_sum)
+
+sum = 0
+
+for count in group_count:
+    sum += count
+
+print (sum)
+
+
+group_count = []
+sum = 0
+for group in groups:
+    common = set(group[0])
+    for person in group:
+        common = common.intersection(set(person))
+
+    group_count.append(len(common))
+    sum += len(common)
+
+sum = 0
+
+for count in group_count:
+    sum += count
+
+print(sum)
