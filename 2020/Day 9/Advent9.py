1e3545704a8550a597be7a21db0d1e70bf55df0f
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-09 04:53:34 -0500

    Day 9 solutions. Part 2 needs more optimization.

--- 2020/Day 9/Advent9.py
+++ 2020/Day 9/Advent9.py
@@ -1,1 +1,54 @@
+cypher = []
+
 with open("input.txt") as f:
+	f = f.read()
+	cypher = list(f.split("\n"))
+
+for i in range(len(cypher)):
+	cypher[i] = int(cypher[i])
+
+def debug(s):
+	if True:
+		print(s)
+	else:
+		pass
+debug(cypher)
+
+def equal_last_x(num, last_x):
+	last_x.sort()
+	i_1 = 0;
+	i_2 = len(last_x) - 1;
+
+	while (last_x[i_1] != last_x[i_2]):
+		sum = last_x[i_1] + last_x[i_2]
+		if sum == num:
+			return True;
+		elif sum < num:
+			i_1 += 1
+		elif sum > num:
+			i_2 -= 1
+
+	return False
+
+last_n = 25
+weakness = 0;
+
+for i in range(last_n, len(cypher)):
+	num = cypher[i]
+	last_25 = cypher[i-last_n : i]
+	val = equal_last_x(num, last_25)
+	if not val:
+		print(num)
+		weakness = num
+		break
+
+
+for i in range(len(cypher)):
+	for j in range(i + 2, len(cypher)):
+		previous = cypher[i:j]
+		sum = 0
+		for n in previous:
+			sum += n
+
+		if sum == weakness:
+			print(previous[0] + previous[-1])
