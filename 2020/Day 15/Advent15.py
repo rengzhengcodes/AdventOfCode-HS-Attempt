0356803a6aa6f47664e5687e595d0d1696b4e1ec
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-15 05:57:47 -0500

    Linearized solution.

--- 2020/Day 15/Advent15.py
+++ 2020/Day 15/Advent15.py
@@ -5,20 +5,22 @@ tests = [[0,3,6], [1,3,2], [2,1,3]]
 
 def get_at_index(numbers, index=2020):
 	last = numbers[-1]
+	numbers = numbers[0:-1]
 	last_occurances = dict()
+	for i in range(len(numbers)):
+		last_occurances[numbers[i]] = (i,i)
+	#print(last_occurances)
 	for i in range(len(numbers), index):
-		#print(last_num, i, numbers)
-		#print(numbers[-1], i)
-		#print(i)
-		without_last = numbers[0:-1]
-		if last not in without_last:
+		#print(last_occurances, i)
+		if last not in last_occurances.keys():
+			numbers.append(last)
+			last_occurances[last] = (i,i)
 			last = 0
 		else:
-			last_occurance = len(numbers) - (without_last[::-1].index(last)) - 1
-			#print(last_occurance, i)
-			last = i - last_occurance
-		numbers.append(last)
+			numbers.append(last)
+			last_occurances[last] = (last_occurances[last][1], i)
+			last = i - last_occurances[last][0]
 	return numbers[-1]
-#for test in tests:
-#	print(get_at_index(2020, test))
-print(get_at_index(input))
+
+#print(get_at_index(tests[0]))
+print(get_at_index(input,30000000))
