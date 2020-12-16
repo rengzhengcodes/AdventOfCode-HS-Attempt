9ad2524956b883d2829c69cc17ceb53a5d57820a
rengzhengcodes <rzhengcontacts@gmail.com>
2020-12-16 02:17:59 -0500

    Part 1 space saved.

--- 2020/Day 16/Advent16.py
+++ 2020/Day 16/Advent16.py
@@ -1,60 +1,83 @@
 parts = open("input.txt").read().split("\n\n")
-ranges_raw = parts[0]
-ranges_raw = list(ranges_raw.split("\n"))
-ranges = dict()
-for range in ranges_raw:
-	field, valids = range.split(": ")
+intervals_raw = parts[0]
+intervals_raw = list(intervals_raw.split("\n"))
+intervals = dict()
+for interval in intervals_raw:
+	field, valids = interval.split(": ")
 	valids = valids.split(" or ")
 	valids = [(int(start), int(end)) for start, end in [valid.split("-") for valid in valids]]
 	#print(valids)
-	ranges[field] = valids
-#print(ranges)
+	intervals[field] = valids
+#print(intervals)
 my_ticket = tuple([int(field) for field in parts[1].split("\n")[1].split(",")])
 
 taken_tickets = [tuple([int(field) for field in ticket.split(",")]) for ticket in parts[2].split("\n")[1:]]
-#print(ranges.values())
+#print(intervals.values())
 
-def in_a_range(value, ranges):
-	for range in ranges:
-		#print(range)
-		if range[0] <= field <= range[1]:#if in range
+def in_a_interval(value, intervals):
+	for interval in intervals:
+		#print(interval)
+		if interval[0] <= value <= interval[1]:#if in intervaltype(interval[0])
 			return True
 			break
+	return False
+
+def in_a_series_of_intervals(value, series):
+	validity = False
+	for serie in series:
+		validity = in_a_interval(value, serie)
+		if validity:
+			return True
 	return False
 
 error_rate = 0
 for ticket in taken_tickets:
 	for field in ticket:
-		for all_ranges in ranges.values():
-			broken = not in_a_range(field, all_ranges)
-			if not broken:
-				break
-		if broken:
+		if not in_a_series_of_intervals(field, intervals.values()):
 			error_rate += field
 print(error_rate)
 
-#discardubg ubvakuds
+#discarding invalid tickets
 valid_tickets = [my_ticket]
 for ticket in taken_tickets:
+	valid = True
 	for field in ticket:
-		broken = True
-		for all_ranges in ranges.values():
-			for range in all_ranges:
-				#print(range)
-				if range[0] <= field <= range[1]:
-					broken = False
-					break
-			if not broken:#if its valid for a field, it passes, no need to check others for validity.
-				break
-		if broken:#if the ticket is already broken, no need to check rest, invalid value
+		if not in_a_series_of_intervals(field, intervals.values()):
+			valid = False
 			break
-		else:
-			valid_tickets.append(ticket)
+	if valid:
+		valid_tickets.append(ticket)
 
-for field, all_ranges in ranges.items():
-	pass
-##	for ticket in tickets:
-##		for field in ticket:
-##			broken = True
-##			for range in all_ranges:
-##				if range[0] <= field <= range[1]
+fields = [[field] for field in valid_tickets[0]]#initial list setup
+
+for ticket in valid_tickets[1:]:
+	for i in range(len(ticket)):
+		fields[i].append(ticket[i])#processing all fields to 1
+
+my_ticket_fields = dict()
+
+print(fields)
+
+def values_in_interval(vals, section):
+	for val in vals:
+		if not in_a_interval(val, section):
+			return False
+	return True
+
+for field, all_intervals in intervals.items():
+	for i in range(len(fields)):
+		correct_field = values_in_interval(fields[i], all_intervals)
+		if correct_field and field not in my_ticket_fields.keys():
+			my_ticket_fields[field] = [my_ticket[i]]
+			break
+		elif correct_field:
+			my_ticket_fields[field].append([my_ticket[i]])
+			break
+
+print(my_ticket_fields)
+
+#product = 1
+#for fields in intervals.keys():
+#	if "departure" in fields:
+#		product *= my_ticket_fields[fields]
+#print(product)
