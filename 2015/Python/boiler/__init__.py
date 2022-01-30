2b15a10fcc97d6dd1c5198e63f516ee68a7d1ddb
rengzhengcodes <rzhengcontacts@gmail.com>
2022-01-30 16:14:56 -0500

    fixed init relative file location

--- 2015/Python/boiler/__init__.py
+++ 2015/Python/boiler/__init__.py
@@ -1,3 +1,6 @@
-with open('session_id.secret', 'r') as sid:
+from os import path
+here = path.dirname(path.abspath(__file__)) + '/'
+
+with open(here + 'session_id.secret', 'r') as sid:
 	session_id = sid.read().rstrip()
 	sid.close()
