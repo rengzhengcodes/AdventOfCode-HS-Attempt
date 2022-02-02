b0b3a744545be67f958959427bfa49394413781c
rengzhengcodes <rzhengcontacts@gmail.com>
2022-02-02 10:57:27 -0500

    part 1 attempt

--- 2015/Python/Day 6/main.py
+++ 2015/Python/Day 6/main.py
@@ -11,8 +11,46 @@ here = path.dirname(path.abspath(__file__)) + '/'
 get_input(here, 2015, 6)
 
 def pt1():
+	#generates light grid
+	grid = list()
+	for i in range(1000):
+		grid.append(list())
+		for j in range(1000):
+			grid[i].append(False)
+
 	with open(here + 'input.txt') as input:
 		for line in input:
+			#gets individual words
 			words = line.split()
-			if words[0] is 'toggle':
+			#extracts start coordinates
+			start = words[-3].split(',')
+			start = [int(x) for x in start]
+			start = tuple(start)
+			#extracts end coordinates
+			end = words[-1].split(',')
+			end = [int(x) for x in start]
+			end = tuple(end)
+
+			if words[0] == 'toggle':
+				for i in range(start[0], end[0] + 1):
+					for j in range(start[1], end[1] + 1):
+						grid[i][j] = not grid[i][j]
 			else: # words[0] is turn
+				if words[1] == 'on':
+					for i in range(start[0], end[0] + 1):
+						for j in range(start[1], end[1] + 1):
+							grid[i][j] = True
+				else: #words[1] is off
+					for i in range(start[0], end[0] + 1):
+						for j in range(start[1], end[1] + 1):
+							grid[i][j] = False
+	#gets number of lights on
+	on_count = 0
+	for i in range(1000):
+		for j in range(1000):
+			if grid[i][j]:
+				on_count += 1
+
+	clipboard(on_count)
+
+pt1()
