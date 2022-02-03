76d19ac68663d686ee9c304bfcbf78e0eb7ef097
rengzhengcodes <rzhengcontacts@gmail.com>
2022-02-03 14:17:28 -0500

    boiler

---
+++ 2015/Python/Day 9/main.py
@@ -0,0 +1,13 @@
+import sys
+from os import path
+# adds repo of boiler
+boiler_parent = path.dirname(path.dirname(path.abspath(__file__)))
+print(boiler_parent)
+sys.path.append(boiler_parent)
+from boiler import *
+
+here = path.dirname(path.abspath(__file__)) + '/'
+
+get_input(here, 2015, 8)
+
+def pt1():
