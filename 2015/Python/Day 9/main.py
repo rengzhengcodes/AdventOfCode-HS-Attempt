95a335c8ef84fe3f6f8d53837f4dea9973a29465
rengzhengcodes <rzhengcontacts@gmail.com>
2022-02-04 08:11:01 -0500

    map made

--- 2015/Python/Day 9/main.py
+++ 2015/Python/Day 9/main.py
@@ -8,6 +8,27 @@ from boiler import *
 
 here = path.dirname(path.abspath(__file__)) + '/'
 
-get_input(here, 2015, 8)
+get_input(here, 2015, 9)
 
 def pt1():
+	path_weights = dict() # dictionary with dictionaries of start and end destinations, end destination keys correlate to weight of path
+	with open(here + 'input.txt') as input:
+		# establishes paths
+		for line in input:
+			line = line.rstrip()
+			parts = line.split()
+			city1 = parts[0]
+			city2 = parts[2]
+			distance = int(parts[4])
+
+			if city1 not in path_weights.keys():
+				path_weights[city1] = dict()
+			if city2 not in path_weights.keys():
+				path_weights[city2] = dict()
+
+			path_weights[city1][city2] = distance
+			path_weights[city2][city1] = distance
+
+	print(path_weights)
+
+pt1()
