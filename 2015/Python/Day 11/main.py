e17ff6dee1687333190cf089d7cbd80f58cc29d4
rengzhengcodes <rzhengcontacts@gmail.com>
2022-02-04 21:40:58 -0500

    setup

---
+++ 2015/Python/Day 11/main.py
@@ -0,0 +1,15 @@
+import sys
+from itertools import groupby
+from os import path
+# adds repo of boiler
+boiler_parent = path.dirname(path.dirname(path.abspath(__file__)))
+print(boiler_parent)
+sys.path.append(boiler_parent)
+from boiler import *
+
+here = path.dirname(path.abspath(__file__)) + '/'
+
+get_input(here, 2015, 10)
+
+def pt1():
+	with open(here + 'input.txt') as input:
